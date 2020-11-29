17a13212be90035fa9aef88536a1a6f5b57e82a1
santiagxf <santiagof@outlook.com>
2020-11-29 21:44:29 +0000

    Generadores de vectores de audio

--- Desafio/Utils/audio_extractor.py
+++ Desafio/Utils/audio_extractor.py
@@ -8,12 +8,23 @@ Profesor: Facundo Santiago
 
 import moviepy
 import moviepy.editor
+import logging
+from tqdm import tqdm
 
 def extract_audio_from_videos(root_dir, target_dir):
     """Extrae el audio en formato wav de un conjunto de videos almacenados en el directorio especificado. Los audios son almacenados
     bajo el mismo nombre de cada archivo en un subdirectorio en target_dir llamado audios."""
-    for file_ in os.listdir(root_dir):
-        video = moviepy.editor.VideoFileClip(os.path.join(root_dir, file_))
-        basename = os.path.splitext(os.path.basename(os.path.join(root_dir, file_)))[0]
-        video.audio.write_audiofile(os.path.join(target_dir, 'audios', basename + ".wav"), codec='pcm_s16le', verbose=False) # codec='pcm_s16le' para formato wav
-        video.close()
+    for file_ in tqdm(os.listdir(root_dir)):
+        try:
+            basename = os.path.splitext(os.path.basename(os.path.join(root_dir, file_)))[0]
+            audio_file = os.path.join(target_dir, 'audios', basename + ".wav")
+            
+            if os.path.isfile(audio_file):
+                logging.debug('Ignoring file {0} cause the wav already exists', file_)
+            else:
+                video = moviepy.editor.VideoFileClip(os.path.join(root_dir, file_))
+                video.audio.write_audiofile(audio_file, codec='pcm_s16le', verbose=False) # codec='pcm_s16le' para formato wav
+                video.close()
+        except:
+            logging.error('Unable to process file: {0}'.format(file_))
+            continue
--- Desafio/Utils/audio_featurizer.py
+++ Desafio/Utils/audio_featurizer.py
@@ -8,9 +8,9 @@ from tqdm import tqdm
 from yamnet.yamnet import yamnet_frames_model
 from yamnet.params import Params
 
-YAMNET_PATH = "Models/yamnet.h5"
+YAMNET_PATH = "Models/Yamnet/yamnet.h5"
 
-def class AudioFeaturizer():
+class AudioFeaturizer():
     def __init__(self, yamnet_weights):
         if os.path.isfile(yamnet_weights):
             self.yamnet_weights = yamnet_weights
@@ -22,19 +22,27 @@ def class AudioFeaturizer():
         model = yamnet_frames_model(Params())
         model.load_weights(YAMNET_PATH)
         for sound in tqdm(os.listdir(path)):
-            wav = librosa.load(os.path.join(path, sound), sr=16000)[0].astype(np.float32)
+            basename = os.path.splitext(sound)[0]
+            extension = os.path.splitext(sound)[1]
+            
+            try:
+                wav = librosa.load(os.path.join(path, sound), sr=16000)[0].astype(np.float32)
 
-            for feature in model(wav)[1]:
-                samples.append(os.path.splitext(sound)[0])
-                features.append(feature)
+                for feature in model(wav)[1]:
+                    samples.append(basename)
+                    features.append(feature)
+            except:
+                logging.error('Unable to process file: {0}'.format(sound))
+                continue
+
         self.samples = np.asarray(samples)
         self.features = np.asarray(features)
 
         return self.samples, self.features
 
-    def audio_features_to_csv(self, file_name='./audio_features.csv'):
-        if self.samples:
-            alldata = np.concatenate((samples.reshape((-1,1)),features), axis=1)
+    def to_csv(self, file_name='./audio_features.csv'):
+        if self.samples.any():
+            alldata = np.concatenate((self.samples.reshape((-1,1)),self.features), axis=1)
             df = pd.DataFrame(data=alldata, columns=['sequence_name', *range(1,1025)])
 
             df.to_csv(file_name)
