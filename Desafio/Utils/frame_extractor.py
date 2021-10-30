e13f579e9b4bd3f9386ac6235dae232054a45e10
santiagxf <santiagof@outlook.com>
2021-10-30 12:48:48 -0400

    docs: desafio

--- Desafio/Utils/frame_extractor.py
+++ Desafio/Utils/frame_extractor.py
@@ -1,25 +1,42 @@
 import cv2
 import os
+import pathlib
+from typing import Optional
 from tqdm import tqdm
 
-def extract_frames(videos_dir, out_dir, rate_sec=2):
-    for file_ in tqdm(os.listdir(videos_dir)):
-        count = 0
-        basename = os.path.splitext(os.path.basename(os.path.join(videos_dir, file_)))[0]
-        targetpath = os.path.join(out_dir, basename)
+def extract_frames(videos_dir: str, out_dir: str, label_in_subfolder: bool, sample_each_seconds: Optional[int], max_sequence_lenght: Optional[int]):
+    pattern = "*/*.avi" if label_in_subfolder else "*.avi"
+
+    if not (sample_each_seconds or max_sequence_lenght):
+        raise ValueError('Al menos sample_each_seconds o max_sequence_lenght debe ser indicado')
+    
+    if sample_each_seconds <= 0 or max_sequence_lenght <= 0:
+        raise ValueError('sample_each_seconds o max_sequence_lenght tienen un valor incorrecto')
+
+    for file_ in tqdm(pathlib.PosixPath(videos_dir).glob(pattern)):
+        basename = file_.name.split('.')[0]
+        basefolder = file_.parent.relative_to(videos_dir)
+        targetpath = os.path.join(out_dir, basefolder, basename)
+
         if os.path.isdir(targetpath):
+            print('[INFO] El siguiente video no se procesará porque ya existe en el destino:', targetpath)
             continue
-        
-        print('Processing:', targetpath)
-        os.mkdir(targetpath)
-        
-        vidcap = cv2.VideoCapture(os.path.join(videos_dir, file_))
-        success,image = vidcap.read()
-        success = True
+
+        os.makedirs(targetpath, exist_ok=True)
+        vidcap = cv2.VideoCapture(str(file_))
+
+        if (sample_each_seconds):
+            sample_every_frame = 1000 * sample_each_seconds
+        else:
+            num_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+            sample_every_frame = max(1, num_frames // max_sequence_lenght)
+
+        success, image = vidcap.read()
+        frame_idx = 0
         while success:
-            cv2.imwrite(os.path.join(targetpath, "%s_frame_%d.jpg" % (basename, count)), image)
-            count = count + 1
-            vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000*rate_sec))
+            cv2.imwrite(os.path.join(targetpath, "%s_frame_%d.jpg" % (basename, frame_idx)), image)
+            frame_idx += 1
+            vidcap.set(cv2.CAP_PROP_POS_MSEC,(frame_idx*sample_every_frame))
             success,image = vidcap.read()
-        
+
         vidcap.release()
--- docs/index.rst
+++ docs/index.rst
@@ -51,5 +51,6 @@ Gestión y análisis de datos no estructurados
    Actividad 1: Modelado clásico <nlp/classic/topic-modeling-activity.ipynb>
    Actividad 2: Modelos basados en secuencias con Word2Vec <nlp/neural/sequences-activity.ipynb>
    Actividad 3: Modelos de detección de objectos <vision/tasks/detection/object_detection.ipynb>
+   Trabajo Final <practice/Memorabilidad.ipynb>
 
 
