12a814618b721571bda21dffd1d068f5ba10c218
Facundo Santiago <santiagof@outlook.com>
2022-10-09 13:24:21 -0400

    Update TextNormalizer.py

--- NLP/Utils/TextNormalizer.py
+++ NLP/Utils/TextNormalizer.py
@@ -16,18 +16,11 @@ from nltk.corpus import stopwords
 from nltk.tokenize.casual import TweetTokenizer
 
 class TweetTextNormalizer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
-    def __init__(self, language='spanish', lemmatize=True, stem=False, reduce_len=True, strip_handles=True, strip_stopwords=True, strip_urls=True, token_min_len=4, preserve_case=True, text_to_sequence=False):
+    def __init__(self, language='spanish', lemmatize=True, stem=False, reduce_len=True, strip_handles=True, strip_stopwords=True, strip_urls=True, strip_accents=True, token_min_len=-1, preserve_case=True, text_to_sequence=False):
         """Un normalizador de texto pensado para procesamiento de Tweets en español. Este normalizador puede devolver o bien un texto transformado o bien una secuencia de tokens si se
         indica el parametro `text_to_sequence`. Dentro de los procesamientos disponibles son lemmatization, stemming, reducir la longitud de caracteres repetidos, eliminar handles, 
         eliminar URLs, eliminar mayusculas."""
-        try:
-            import es_core_news_sm as spa
-            
-            self.parser = spa.load() # Cargamos el parser en español
-        except ImportError:
-            raise ImportError('El modelo en español no está instalado. Ejecute python -m spacy download en_core_web_sm')
         
-        nltk.download("stopwords", quiet=True)
         self.tokenizer = TweetTokenizer(strip_handles=strip_handles, 
                                         reduce_len=reduce_len,
                                         preserve_case=preserve_case) # Creamos un tokenizer
@@ -37,11 +30,21 @@ class TweetTextNormalizer(sklearn.base.BaseEstimator, sklearn.base.TransformerMi
             self.stemmer = False
         
         if lemmatize:
+            try:
+                import es_core_news_sm as spa
+            
+                self.parser = spa.load() # Cargamos el parser en español
+            except ImportError:
+                raise ImportError('El modelo en español no está instalado. Ejecute python -m spacy download en_core_web_sm')
             self.lemmatizer = lambda word : " ".join([token.lemma_ for token in self.parser(word)]) # Creamos un lemmatizer
         else:
             self.lemmatizer = False
         
-        self.strip_stopwords = strip_stopwords
+        if strip_stopwords:
+            nltk.download("stopwords", quiet=True)
+            self.strip_stopwords = strip_stopwords
+
+        self.strip_accents = strip_accents
         self.strip_urls = strip_urls
         self.stopwords = set(stopwords.words(language)) # Instanciamos las stopwords en español
         self.urls_regex = re.compile('http\S+') # Usamos una expresion regular para encontrar las URLs
@@ -55,10 +58,10 @@ class TweetTextNormalizer(sklearn.base.BaseEstimator, sklearn.base.TransformerMi
 
         if (self.strip_urls):
             tokens = [token for token in tokens if not re.match(self.urls_regex, token)]
-        
-        tokens = [token for token in tokens if len(token) > self.token_min_len]
-        tokens = [unidecode.unidecode(token) for token in tokens] # Quitamos acentos
-
+        if (self.token_min_len > 1)
+            tokens = [token for token in tokens if len(token) > self.token_min_len]
+        if (self.strip_accents)
+            tokens = [unidecode.unidecode(token) for token in tokens]
         if (self.strip_stopwords):
             tokens = [token for token in tokens if token not in self.stopwords]
         if self.lemmatizer:
